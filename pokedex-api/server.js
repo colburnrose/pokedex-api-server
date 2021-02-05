a83c3355f9e8a919920fba767d5d19405048b833
colburncodes <colburnsanders@gmail.com>
2021-02-04 22:13:26 -0600

    cleaup before deploy

--- pokedex-api/server.js
+++ pokedex-api/server.js
@@ -7,7 +7,9 @@ const POKEDEX = require("./pokedex");
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganConfiguration =
+  process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganConfiguration));
 app.use(helmet());
 app.use(cors());
 
@@ -37,7 +39,6 @@ app.use(function validateBearerToken(req, res, next) {
   const authToken = req.get("Authorization");
   const apiToken = process.env.API_TOKEN;
 
-  console.log("validate bearer token authentication");
   // validate that token exists
   // check to see if authToken doesn't match apiToken
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
@@ -75,7 +76,17 @@ app.get("/pokemon", function handleGetPokemeon(req, res) {
   res.json(response);
 });
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response = "";
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "Server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
